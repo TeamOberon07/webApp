c4e0577dcdf60fc04473e7fc5d6dc9a9c2c1762e
Xida <xidachen3@gmail.com>
2022-05-21 18:26:57 +0200

    Fix test orderpage + register seller + header

--- src/components/__tests__/webApp_tests/Header.test.js
+++ src/components/__tests__/webApp_tests/Header.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from "@testing-library/react"
 import { BrowserRouter } from "react-router-dom";
 import { Header } from "../../Header"
+import { StateContext} from "../../StateContext"
 
 test("visualizzazione address utente", ()=>{
-    render(<BrowserRouter><Header currentAddress ="0xe5B197D91ad002a18917aB4fdc6b6E0126797482" ></Header></BrowserRouter>);
-    expect(screen.getByText("0xe5B1...797482"));
+    
+    render(<StateContext.Provider value={ {
+       currentAddress:"0xe5B197D91ad002a18917aB4fdc6b6E0126797482",
+       currentBalance:"",
+       contractAddress:"",
+       networks:{"rinkeby":{"chainId":"0x4","chainName":"Ethereum Rinkeby Testnet","nativeCurrency":{"name":"AVAX","symbol":"AVAX","decimals":18},"rpcUrls":["https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"],"blockExplorerUrls":["https://rinkeby.etherscan.io"]}},
+
+        ourNetwork: "rinkeby",
+       _cutAddress:() =>{ return "0xe5B1...797482"},
+        _connectWallet:() =>{},
+       _setListenerMetamaskAccount:() =>{},
+ 
+     }}>
+       <BrowserRouter>
+            <Header></Header>
+       </BrowserRouter>
+   </StateContext.Provider>)
+    
+    expect(screen.getAllByText("0xe5B1...797482"));
 })
